e3299116a40d654d1e0460091ba31018e8149e23
Andre Vaz <andre.vaz1411@gmail.com>
2022-01-10 21:08:49 +0000

    update

--- src/tests/Teste.java
+++ src/tests/Teste.java
@@ -7,11 +7,15 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 import java.util.UUID;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -30,32 +34,43 @@ public class Teste {
         }).start();
 
         this.botClient = new BotClient("admin");
-        boolean b = this.botClient.insertFlight("Porto", "Lisboa", 10);
-        assertTrue(b);
     }
 
     @Test
     void insertFlight() {
-        for (int i = 0; i < 200; i++) {
-            new Thread(() -> {
+        List<Thread> threads = new ArrayList<>();
+        for (int i = 0; i < 20; i++) {
+            Thread thread = new Thread(() -> {
                 try {
                     BotClient bot = new BotClient("admin");
-                    bot.insertFlight(
-                            RandomUtils.randomString(),
-                            RandomUtils.randomString(),
-                            RandomUtils.randomInt(1, 20)
-                    );
+                    for (int j = 0; j < 500; j++) {
+                        bot.insertFlight(
+                                RandomUtils.randomString(),
+                                RandomUtils.randomString(),
+                                RandomUtils.randomInt(1, 20)
+                        );
+                    }
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
-            }).start();
+            });
+            thread.start();
+            threads.add(thread);
         }
 
-
+        for(Thread t : threads){
+            try {
+                t.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        List<Tuple<String, String>> voos = this.botClient.getFlights();
         System.out.println("\t\t****** Lista de voos disponíveis ******");
-        for (Tuple<String, String> tup : this.botClient.getFlights()) {
+        for (Tuple<String, String> tup : voos) {
             System.out.println("Origem -> " + tup.getX() + "  Destino -> " + tup.getY());
         }
+        assertEquals(20 * 500, voos.size());
     }
 
     @AfterAll
